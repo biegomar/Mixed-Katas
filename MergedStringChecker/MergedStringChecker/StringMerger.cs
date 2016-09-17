a14e2aecb5f8a29d59f446d6697bd27e3185c4fd
Marc Biegota <marc.biegota@5minds.de>
2016-09-17 20:14:00 +0200

    Stub for the kata Merged String Checker

---
+++ MergedStringChecker/MergedStringChecker/StringMerger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergedStringChecker
+{
+    public class StringMerger
+    {
+        public static bool isMerge(string s, string part1, string part2)
+        {
+            return false;
+        }
+    }
+}
