be566c95d4aca48f92508606e22f2d4e7be04b17
Marc Biegota <marc.biegota@5minds.de>
2016-09-17 10:39:25 +0200

    Initiale Version des Kata SplitString

---
+++ SplitString/SplitString/SplitString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitString
+{
+    using System.Diagnostics;
+
+    public class SplitString
+    {
+        public static string[] Solution(string str)
+        {
+            var result = new List<string>();
+            var pairCounter = 1;
+            var pair = string.Empty;
+            
+            foreach (var item in str)
+            {
+                if (pairCounter <= 2)
+                {
+                    pair = pair + item;
+                    pairCounter++;
+                }
+                else
+                {
+                    result.Add(pair);
+                    pair = item.ToString();
+                    pairCounter = 1;
+                }
+            }
+
+            if (pairCounter == 1)
+            {
+                result.Add(pair + "_");
+            }
+
+            return result.ToArray();
+        }
+    }
+}
