0028b696bc512a7ce770b1bca2c4f66763ade3d5
Marc Biegota <marc.biegota@5minds.de>
2016-09-17 17:20:06 +0200

    Kata-VasyaClerk (not ready yet)
    The last testcase on codewars is still red.

---
+++ VasyaClerk/VasyaClerk/Line.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VasyaClerk
+{
+    public class Line
+    {
+        public static string Tickets(int[] peopleInLine)
+        {
+            var pocket = 0;
+
+            foreach (var people in peopleInLine)
+            {
+                pocket -= (people - 25);
+                if (pocket < 0)
+                {
+                    return "NO";
+                }
+                pocket += 25;
+            }
+
+            return "YES";
+        }
+    }
+}
