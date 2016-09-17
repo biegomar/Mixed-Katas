720916b6a585c9e5807b0880414e645f6d7734c5
Marc Biegota <marc.biegota@5minds.de>
2016-09-17 23:17:14 +0200

    Implementation of the kata Palindrome Chain Length

---
+++ PalindromeChainLength/PalindromeChainLength.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PalindromeChainLength.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Given87OutputShouldBe4()
+        {
+            Assert.AreEqual(4, Kata.palindromeChainLength(87));
+        }
+
+        [TestMethod]
+        public void Given88OutputShouldBe0()
+        {
+            Assert.AreEqual(0, Kata.palindromeChainLength(88));
+        }
+    }
+}
