b2e0348fa9342225b0eb9c9c6095b514abb3cfbe
Marc Biegota <marc.biegota@5minds.de>
2016-09-17 16:46:08 +0200

    Added Kata-PersistentBugger

---
+++ PersistentBugger/PersistentBugger.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PersistentBugger.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(3, Persist.Persistence(39));
+            Assert.AreEqual(0, Persist.Persistence(4));
+            Assert.AreEqual(2, Persist.Persistence(25));
+            Assert.AreEqual(4, Persist.Persistence(999));
+        }
+    }
+}
