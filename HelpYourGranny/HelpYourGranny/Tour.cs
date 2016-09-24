49f2d97de84620f2acbdc7bcb922ce3364696f1e
Marc Biegota <marc.biegota@5minds.de>
2016-09-24 21:47:21 +0200

    Initialize Kata HelpYourGranny

---
+++ HelpYourGranny/HelpYourGranny/Tour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpYourGranny
+{
+    using System.Collections;
+
+    public class Tour
+    {
+        public static int tour(string[] arrFriends, string[][] ftwns, Hashtable h)
+        {
+            foreach (var friend in arrFriends)
+            {
+                var town = ftwns.FirstOrDefault(ftwn => ftwn[0] == friend);
+                if (town != null)
+                {
+                    var distance = h[town[1]];
+                }
+            }
+            return -1;
+        }
+    }
+}
