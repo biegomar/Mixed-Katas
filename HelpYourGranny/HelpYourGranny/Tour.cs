7986d7845239ab5e5fbee0d8d8dbad2b9f3d4339
Marc Biegota <marc.biegota@5minds.de>
2016-09-24 22:54:23 +0200

    Finished kata HelpYourGranny

--- HelpYourGranny/HelpYourGranny/Tour.cs
+++ HelpYourGranny/HelpYourGranny/Tour.cs
@@ -7,20 +7,43 @@ using System.Threading.Tasks;
 namespace HelpYourGranny
 {
     using System.Collections;
+    using System.Diagnostics;
 
     public class Tour
     {
         public static int tour(string[] arrFriends, string[][] ftwns, Hashtable h)
         {
+            var fromTown = "X0";
+            double totalDistance = 0;
             foreach (var friend in arrFriends)
             {
                 var town = ftwns.FirstOrDefault(ftwn => ftwn[0] == friend);
                 if (town != null)
                 {
-                    var distance = h[town[1]];
+                    var toTown = town[1];
+                    var distance = Convert.ToInt32(h[toTown]);
+                    
+                    if (fromTown == "X0")
+                    {
+                        totalDistance += distance;                        
+                    }
+                    else
+                    {
+                        var a = Convert.ToInt32(h[fromTown]);
+                        var c = distance;
+                        totalDistance += CalcRoute(a, c);
+                    }
+                    fromTown = toTown;
                 }
-            }
-            return -1;
+            }            
+            var closeCircuitDistance =  Convert.ToInt32(h[fromTown]);
+            
+            return Convert.ToInt32(Math.Truncate(totalDistance + closeCircuitDistance));
+        }
+
+        private static double CalcRoute(int a, int c)
+        {            
+            return Math.Sqrt(c * c - a * a);
         }
     }
 }
