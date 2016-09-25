8408de919fd75126bfe6b1d6d727eb2505501865
Marc Biegota <marc.biegota@5minds.de>
2016-09-25 17:14:48 +0200

    Kata Area of circle finished

---
+++ AreaOfCircle/AreaOfCircle.Test/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AreaOfCircle.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void ShouldFailNegativeNumber()
+        {
+            Kata.CalculateAreaOfCircle("-123");
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void ShouldFailAlphaNumeric()
+        {
+            Kata.CalculateAreaOfCircle("number");
+        }
+
+        [TestMethod]
+        public void ShouldPass1()
+        {
+            Assert.AreEqual(5881.25, Kata.CalculateAreaOfCircle("43.2673"));
+        }
+
+        [TestMethod, ExpectedException(typeof(ArgumentException))]
+        public void WrongFormat()
+        {
+            Kata.CalculateAreaOfCircle("43,2673");
+        }
+    }
+}
