62b975bff3ef0331f16b4ab8ca080aa6cd9bab53
Marc Biegota <marc.biegota@5minds.de>
2016-09-25 11:16:46 +0200

    Initialize kata BalanceParentheses

---
+++ BalanceParentheses/BalanceParantheses.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BalanceParantheses.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BalanceParentheses/BalanceParentheses/Kata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalanceParentheses
+{
+    public class Kata
+    {
+        public static bool IsBalanced(string s, string caps)
+        {
+            return false;
+        }
+    }
+}
