57e5e77edeaf4f152b06c903b407cb4aed2958ce
Marc Biegota <marc.biegota@5minds.de>
2016-09-25 11:52:49 +0200

    Unsatisfying intermediate result

--- BalanceParentheses/BalanceParantheses.Test/UnitTest1.cs
+++ BalanceParentheses/BalanceParantheses.Test/UnitTest1.cs
@@ -3,12 +3,22 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BalanceParantheses.Test
 {
+    using BalanceParentheses;
+
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(true, Kata.IsBalanced("(Sensei says yes!)", "()"));
+            Assert.AreEqual(false, Kata.IsBalanced("(Sensei says no!", "()"));
+
+            Assert.AreEqual(true, Kata.IsBalanced("(Sensei [says] yes!)", "()[]"));
+            Assert.AreEqual(false, Kata.IsBalanced("(Sensei [says) no!]", "()[]"));
+
+            Assert.AreEqual(true, Kata.IsBalanced("Sensei says -yes-!", "--"));
+            Assert.AreEqual(false, Kata.IsBalanced("Sensei -says no!", "--"));
         }
     }
 }
--- BalanceParentheses/BalanceParentheses/Kata.cs
+++ BalanceParentheses/BalanceParentheses/Kata.cs
@@ -10,6 +10,47 @@ namespace BalanceParentheses
     {
         public static bool IsBalanced(string s, string caps)
         {
+            var acceptedCaps = string.Empty;
+            var index = 0;
+            foreach (var item in caps)
+            {
+                if (index % 2 == 0)
+                {
+                    acceptedCaps += item;
+                }
+                index++;
+            }
+            return recursiveBalancer(s, caps, acceptedCaps);
+        }
+
+        private static bool recursiveBalancer(string s, string caps, string acceptedCaps)
+        {
+            var index = 0;
+            while (!caps.Contains(s[index]))
+            {
+                index++;
+            }
+
+            if (acceptedCaps.Contains(s[index]))
+            {
+                var newAcceptedCaps = string.Empty;
+                var capsIndex = 0;
+                foreach (var item in caps)
+                {
+                    if (capsIndex % 2 == 0)
+                    {
+                        newAcceptedCaps += item;
+                        if (s[capsIndex] == item)
+                        {
+                            newAcceptedCaps += caps[capsIndex + 1];
+                        }
+                    }
+                    capsIndex++;
+                }
+
+                return recursiveBalancer(s.Substring(index + 1), caps, newAcceptedCaps);
+            }
+
             return false;
         }
     }
