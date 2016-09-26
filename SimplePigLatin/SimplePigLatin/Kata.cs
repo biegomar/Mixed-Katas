6cb9bf9c110f0e6806212652655da3c548dd1992
Marc Biegota <marc.biegota@5minds.de>
2016-09-26 21:36:35 +0200

    Finished SimplePigLatin

---
+++ SimplePigLatin/SimplePigLatin/Kata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePigLatin
+{
+    public class Kata
+    {
+        public static string PigIt(string str)
+        {
+            var tokens = str.Split(' ');
+            return tokens.Aggregate(string.Empty, (current, token) => current + (token.Substring(1) + token[0] + "ay ")).Trim();            
+        }
+    }
+}
