82ab93397275414d611cadee3cca6e2eae926f60
Marc Biegota <marc.biegota@5minds.de>
2016-10-11 00:04:45 +0200

    First try of kata

---
+++ NextBiggerNumber/NextBiggerNumber.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NextBiggerNumber.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1234567908, Kata.NextBiggerNumber(1234567890));
+            Assert.AreEqual(21, Kata.NextBiggerNumber(12));
+            Assert.AreEqual(531, Kata.NextBiggerNumber(513));
+            Assert.AreEqual(2071, Kata.NextBiggerNumber(2017));
+            Assert.AreEqual(441, Kata.NextBiggerNumber(414));
+            Assert.AreEqual(414, Kata.NextBiggerNumber(144));            
+            //1234567890
+        }
+    }
+}
---
+++ NextBiggerNumber/NextBiggerNumber/Kata.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextBiggerNumber
+{
+    public class Kata
+    {
+        public static long NextBiggerNumber(long n)
+        {
+            long result = -1;
+            var tokens = n.ToString().ToList();
+            for (int i = n.ToString().Length - 1; i > 0; i--)
+            {
+                if (Convert.ToInt16(tokens[i]) > Convert.ToInt16(tokens[i-1]))
+                {
+                    var swap = tokens[i];
+                    tokens[i] = tokens[i - 1];
+                    tokens[i - 1] = swap;
+
+                    var sortArray = new char[tokens.Count - i];
+                    tokens.CopyTo(i, sortArray, 0, tokens.Count - i);
+                    var sortList = sortArray.ToList();
+                    sortList.Sort();
+                    var resultList = new char[tokens.Count];
+                    tokens.CopyTo(0, resultList, 0, i);
+                    sortList.CopyTo(0, resultList, i, sortList.Count);
+                    return Convert.ToInt64(new string(resultList.ToArray()));
+                }
+            }
+            return result;
+        }
+    }
+}
