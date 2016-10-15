a20840ab76eedfadade91ffc67f8c16aeebd9b8f
Marc Biegota <marc.biegota@5minds.de>
2016-10-15 17:53:28 +0200

    GetTheLoop kata
    Solution with recursion. Will fail with stack overflow on huge graphs.

---
+++ GetTheLoop/GetTheLoop.Test/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GetTheLoop.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FourNodesWithLoopSize3()
+        {
+            var n1 = new LoopDetector.Node();
+            var n2 = new LoopDetector.Node();
+            var n3 = new LoopDetector.Node();
+            var n4 = new LoopDetector.Node();
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4;
+            n4.next = n2;
+            Assert.AreEqual(3, Kata.getLoopSize(n1));
+        }
+    }
+}
---
+++ GetTheLoop/GetTheLoop/Kata.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTheLoop
+{    
+    public class Kata
+    {
+        private static List<LoopDetector.Node> list = new List<LoopDetector.Node>();  
+        public static int getLoopSize(LoopDetector.Node startNode)
+        {
+            var loopEntry = getLoopEntryNode(startNode);            
+            var diff = getCountFromStartToLoopEntry(startNode, loopEntry.Item2);
+            return loopEntry.Item1 - diff;
+        }
+
+        private static Tuple<int, LoopDetector.Node> getLoopEntryNode(LoopDetector.Node startNode)
+        {
+            if (!list.Contains(startNode))
+            {
+                list.Add(startNode);
+                var result = getLoopEntryNode(startNode.next);
+                return new Tuple<int, LoopDetector.Node>(result.Item1 + 1, result.Item2);
+            }
+            return new Tuple<int, LoopDetector.Node>(0, startNode);
+        }
+
+        private static int getCountFromStartToLoopEntry(LoopDetector.Node startNode, LoopDetector.Node loopEntryNode)
+        {            
+            if (startNode != loopEntryNode)
+            {                
+                return 1 + getCountFromStartToLoopEntry(startNode.next, loopEntryNode);
+            }
+            return 0;
+        }
+    }
+}
