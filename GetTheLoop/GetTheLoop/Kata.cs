1491ef8cb45280dafdb9da668a9c793aa440d910
Marc Biegota <marc.biegota@5minds.de>
2016-10-15 21:02:10 +0200

    Small, simple and not recursive

--- GetTheLoop/GetTheLoop/Kata.cs
+++ GetTheLoop/GetTheLoop/Kata.cs
@@ -7,33 +7,20 @@ using System.Threading.Tasks;
 namespace GetTheLoop
 {    
     public class Kata
-    {
-        private static List<LoopDetector.Node> list = new List<LoopDetector.Node>();  
+    {        
         public static int getLoopSize(LoopDetector.Node startNode)
         {
-            var loopEntry = getLoopEntryNode(startNode);            
-            var diff = getCountFromStartToLoopEntry(startNode, loopEntry.Item2);
-            return loopEntry.Item1 - diff;
-        }
-
-        private static Tuple<int, LoopDetector.Node> getLoopEntryNode(LoopDetector.Node startNode)
-        {
-            if (!list.Contains(startNode))
+            var dict = new Dictionary<LoopDetector.Node, int>();
+            var count = 0;
+            var node = startNode;
+            while (!dict.ContainsKey(node))
             {
-                list.Add(startNode);
-                var result = getLoopEntryNode(startNode.next);
-                return new Tuple<int, LoopDetector.Node>(result.Item1 + 1, result.Item2);
+                dict.Add(node, count);
+                node = node.next;
+                count++;
             }
-            return new Tuple<int, LoopDetector.Node>(0, startNode);
-        }
 
-        private static int getCountFromStartToLoopEntry(LoopDetector.Node startNode, LoopDetector.Node loopEntryNode)
-        {            
-            if (startNode != loopEntryNode)
-            {                
-                return 1 + getCountFromStartToLoopEntry(startNode.next, loopEntryNode);
-            }
-            return 0;
-        }
+            return count - dict[node];
+        }           
     }
 }
