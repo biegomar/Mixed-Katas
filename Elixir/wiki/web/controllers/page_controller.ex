f3af04fc6e6144a3e7a51d37473a7169156d4ff0
Marc Biegota <marc.biegota@5minds.de>
2016-12-03 14:41:21 +0100

    Zwischenstand

--- Elixir/wiki/web/controllers/page_controller.ex
+++ Elixir/wiki/web/controllers/page_controller.ex
@@ -2,6 +2,18 @@ defmodule Wiki.PageController do
   use Wiki.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    pages = Repo.all(Wiki.Page)
+    render conn, "index.html", pages: pages
+  end
+
+#def show(conn, params) do
+#  page = Repo.get_by!(Wiki.Page, title: params["id"])
+#  # render show here and pass page
+#end
+#===> das nächste Beispiel nutzt Pattern-Matching. Filtert so nur den genutzten Param aus params!
+
+  def show(conn, %{"id" => id}) do
+    page = Repo.get_by!(Wiki.Page, title: id)
+    render conn, "show.html", page: page
   end
 end
